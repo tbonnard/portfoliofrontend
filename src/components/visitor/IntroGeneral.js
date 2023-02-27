3c3ddb49f919e2ebcc6172133dc930039dbb4de0
tbonnard <thibaud.bonnard@gmail.com>
2023-02-27 19:10:21 +0100

    intro text

--- src/components/visitor/IntroGeneral.js
+++ src/components/visitor/IntroGeneral.js
@@ -5,7 +5,7 @@ const IntroGeneral = () => {
  
         return (
         <div className="introTextGeneral">
-            <p>Je souhaite évoluer vers un poste de développeur junior fullstack et continuer de monter en compétences suite à une solide expérience comme analyste d’affaires/fonctionnel TI Agile. Vous pouvez ainsi découvrir mon profil et quelques-unes de mes réalisations !</p>
+            <p>Suite à une solide expérience comme analyste d’affaires/fonctionnel TI Agile, je recherche, après plusieurs formations, un poste de développeur junior fullstack afin de continuer de monter en compétences. Vous pouvez ainsi découvrir mon profil et quelques-unes de mes réalisations !</p>
         </div>
         )
     }
