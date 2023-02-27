81ce9dbebb00ffafe8212b18f2833f4bfdb42f15
tbonnard <thibaud.bonnard@gmail.com>
2023-02-27 15:43:15 +0100

    notif css

--- src/components/visitor/IntroGeneral.js
+++ src/components/visitor/IntroGeneral.js
@@ -5,7 +5,7 @@ const IntroGeneral = () => {
  
         return (
         <div className="introTextGeneral">
-            <p>Actuellement à la recherche d'un poste de developpeur junior fullstack et à monter en compétences, ce portfolio vous permet d'avoir une première idée sur mon profil !</p>
+            <p>Actuellement à la recherche d'un poste de developpeur junior fullstack et souhaitant monter en compétences, ce portfolio vous permet d'avoir une première idée sur mon profil !</p>
         </div>
         )
     }
