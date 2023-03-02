1fb17827057aa718ac14af23addae0c3c71cbf73
tbonnard <thibaud.bonnard@gmail.com>
2023-03-02 23:34:08 +0100

    visitorid

--- src/services/visitor.js
+++ src/services/visitor.js
@@ -3,16 +3,9 @@ import axios from 'axios'
 const baseUrl = 'https://tbonnard.pythonanywhere.com/api/visitor/'
 
 const getVisitorDetails = async (id) => {
-  const userToken = document.cookie.replace(/(?:(?:^|.*;\s*)jwtTk\s*=\s*([^;]*).*$)|^.*$/, '$1');
-  const csrftoken = window.localStorage.getItem('csrftoken')
-    const response = await axios.get(`${baseUrl}${id}`,
-    {
-      headers: {
-        Authorization: `Bearer ${userToken}`, // Send the token with the request
-        'X-CSRFToken': csrftoken.csrftoken,
-        'Content-Type': 'application/json',
-      }
-    })
+    const response = await axios.get(`${baseUrl}${id}`)
+    console.log(response)
+    console.log(response.data)
     return response.data
 }
 
