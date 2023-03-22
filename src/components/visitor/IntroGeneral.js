3a117b3cb2eaba4b678d92d724482e196ee25365
tbonnard <thibaud.bonnard@gmail.com>
2023-03-22 12:46:31 +0100

    desc

--- src/components/visitor/IntroGeneral.js
+++ src/components/visitor/IntroGeneral.js
@@ -5,7 +5,7 @@ const IntroGeneral = () => {
  
         return (
         <div className="introTextGeneral">
-            <p>Suite à 15 ans de solide expérience comme analyste d’affaires / fonctionnel TI & PO sur des projets Agile, je recherche, après plusieurs formations et projets personnels, un poste de développeur web junior (backend / fullstack) afin de continuer de monter en compétences et aider. Vous pouvez ainsi découvrir mon profil et quelques-unes de mes réalisations !</p>
+            <p>Suite à 15 ans de solide expérience en TI comme analyste d’affaires / fonctionnel & PO sur des projets Agile, je recherche, après plusieurs formations et projets personnels, un poste de développeur web junior (backend / fullstack) afin de continuer de monter en compétences et aider. Vous pouvez ainsi découvrir mon profil et quelques-unes de mes réalisations !</p>
         </div>
         )
     }
