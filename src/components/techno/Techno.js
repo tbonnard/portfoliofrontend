837ef597e21835e5191c7b08deefe463d4ed7d70
tbonnard <thibaud.bonnard@gmail.com>
2023-04-03 12:21:02 +0200

    img tech

--- src/components/techno/Techno.js
+++ src/components/techno/Techno.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import js from '../../files/techno/js.png'
-import python from '../../files/techno/python.png'
+import python from '../../files/techno/python_.png'
 import react from '../../files/techno/react.png'
 import html_css from '../../files/techno/html_css.png'
 import git from '../../files/techno/git.png'
@@ -9,7 +9,7 @@ import django from '../../files/techno/django.png'
 import rest from '../../files/techno/rest.png'
 import node from '../../files/techno/node.png'
 import db from '../../files/techno/db.png'
-
+import csharp from '../../files/techno/csharp.png'
 
 const Techno = () => {
 
@@ -18,13 +18,14 @@ const Techno = () => {
                 <h2 className="titreSection">Technos</h2>
                 <div className="technoItems">
                     <img className="technoItem" src={js} alt='JavaScript' title='JavaScript' style={{backgroundColor: "#3f3958"}}/>
-                    <img className="technoItem" src={react} alt='React' title='React' style={{backgroundColor: "#f9c5aa"}}/>
+                    <img className="technoItem" src={csharp} alt='csharp' title='C#' style={{backgroundColor: "#f9c5aa"}}/>
                     <img className="technoItem" src={python} alt='Python' title='Python'/>
-                    <img className="technoItem" src={html_css} alt='HTLM_CSS' title='HTLM CSS' style={{backgroundColor: "#fbe6e9"}}/>
+                    <img className="technoItem" src={react} alt='React' title='React' style={{backgroundColor: "#9ed544"}}/>
                     <img className="technoItem" src={django} alt='Django Framework' title='Django Framework' style={{backgroundColor: "#47a5e6"}}/>
+                    <img className="technoItem" src={html_css} alt='HTLM_CSS' title='HTLM CSS' style={{backgroundColor: "#180000"}}/>
                     <img className="technoItem" src={rest} alt='Rest API' title='Rest API' style={{backgroundColor: "#f9c5aa"}}/>
                     <img className="technoItem" src={db} alt='SQL MongoDb' title='SQL MongdoDb' style={{backgroundColor: "#47a5e6"}}/>
-                    <img className="technoItem" src={node} alt='nodeJS' title='nodeJS' style={{backgroundColor: "#282b23"}}/>
+                    {/* <img className="technoItem" src={node} alt='nodeJS' title='nodeJS' style={{backgroundColor: "#282b23"}}/>*/}  
                     <img className="technoItem" src={git} alt='git' title='git' style={{backgroundColor: "#3973e7"}}/>
                 </div>
         </div>
