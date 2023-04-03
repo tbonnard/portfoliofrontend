903ee2bb1e59a60e8dc8cf1e0526fce8fd24e537
tbonnard <thibaud.bonnard@gmail.com>
2023-04-03 11:08:27 +0200

    desc

--- src/components/visitor/IntroGeneral.js
+++ src/components/visitor/IntroGeneral.js
@@ -5,7 +5,10 @@ const IntroGeneral = () => {
  
         return (
         <div className="introTextGeneral">
-            <p>Suite à 15 ans de solide expérience en TI comme analyste d’affaires / fonctionnel & PO sur des projets Agile, je recherche, après plusieurs formations et projets personnels, un poste de développeur web junior (backend / fullstack) afin de continuer de monter en compétences et aider. Vous pouvez ainsi découvrir mon profil et quelques-unes de mes réalisations !</p>
+            {/* <p>Suite à 15 ans de solide expérience en TI comme analyste d’affaires / fonctionnel & PO sur des projets Agile, je recherche, après plusieurs formations et projets personnels, un poste de développeur web junior (backend / fullstack) afin de continuer de monter en compétences et aider. Vous pouvez ainsi découvrir mon profil et quelques-unes de mes réalisations !</p> */}
+            <p>
+            Analyste d’affaires/fonctionnel TI et gestionnaire de plus de 10 ans d’expérience en projets TI , la technologie a toujours été au coeur de mon parcours. Un de mes grands atouts est de profondément apprécier le fonctionnement d’un produit, sa logique, son architecture.  Le développement en fait ainsi la voie logique et centrale à mon parcours professionnel et aspirations. Ces différentes formations (C#, Python, Django, JavaScript, Reactjs) et projets (voir ci dessous) m'amène à rechercher un emploi en tant que développeur logiciel / web junior qui bénéficie déjà d'une vision macro aidant à la communication, au travail en équipe et d’une expertise de gestion de projets.
+            </p>
         </div>
         )
     }
